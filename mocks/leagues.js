4e9d06fbb97edbaaf11ea2c684d96e2d228f6489
John Hamm <johndhamm1@gmail.com>
2020-07-18 12:47:25 -0500

    feat(api, mocks): add /leagues-list and /login with leagues and owners mocks

---
+++ mocks/leagues.js
@@ -0,0 +1,112 @@
+module.exports.mockLeagues = [
+  {
+    id: "Nashville_Volleyball-1",
+    name: "Nashville Volleyball",
+    settings: {
+      ppr: false,
+      position_slots: [
+        {
+          position: "QB",
+          total: 2,
+        },
+        {
+          position: "RB",
+          total: 4,
+        },
+        {
+          position: "WR",
+          total: 4,
+        },
+        {
+          position: "TE",
+          total: 2,
+        },
+        {
+          position: "D",
+          total: 2,
+        },
+        {
+          position: "K",
+          total: 2,
+        },
+      ],
+    },
+    status: "closed",
+    draft_order: [
+      "John-Nashville_Volleyball-1",
+      "Rayne-Nashville_Volleyball-1",
+      "Homer-Nashville_Volleyball-1",
+      "Jim-Nashville_Volleyball-1",
+    ],
+  },
+  {
+    id: "Angelas_Gang-1",
+    name: "Angela's Gang",
+    settings: {
+      ppr: false,
+      position_slots: [
+        {
+          position: "QB",
+          total: 1,
+        },
+        {
+          position: "RB",
+          total: 2,
+        },
+        {
+          position: "WR",
+          total: 2,
+        },
+        {
+          position: "TE",
+          total: 1,
+        },
+        {
+          position: "D",
+          total: 1,
+        },
+        {
+          position: "K",
+          total: 1,
+        },
+      ],
+    },
+    status: "closed",
+    draft_order: [],
+  },
+  {
+    id: "Third_League-1",
+    name: "Third League",
+    settings: {
+      ppr: true,
+      position_slots: [
+        {
+          position: "QB",
+          total: 2,
+        },
+        {
+          position: "RB",
+          total: 3,
+        },
+        {
+          position: "WR",
+          total: 3,
+        },
+        {
+          position: "TE",
+          total: 2,
+        },
+        {
+          position: "D",
+          total: 2,
+        },
+        {
+          position: "K",
+          total: 2,
+        },
+      ],
+    },
+    status: "closed",
+    draft_order: [],
+  },
+]
---
+++ mocks/owners.js
@@ -0,0 +1,44 @@
+module.exports.mockOwners = [
+  {
+    id: "John-Nashville_Volleyball-1",
+    name: "John",
+    leagueId: "Nashville_Volleyball-1",
+    isCommish: true,
+  },
+  {
+    id: "Homer-Nashville_Volleyball-1",
+    name: "Homer",
+    leagueId: "Nashville_Volleyball-1",
+    isCommish: true,
+  },
+  {
+    id: "Rayne-Nashville_Volleyball-1",
+    name: "Rayne",
+    leagueId: "Nashville_Volleyball-1",
+    isCommish: false,
+  },
+  {
+    id: "Jim-Nashville_Volleyball-1",
+    name: "Jim",
+    leagueId: "Nashville_Volleyball-1",
+    isCommish: false,
+  },
+  {
+    id: "Angela-Angelas_Gang-1",
+    name: "Angela",
+    leagueId: "Angelas_Gang-1",
+    isCommish: true,
+  },
+  {
+    id: "Homer-Angelas_Gang-1",
+    name: "Homer",
+    leagueId: "Angelas_Gang-1",
+    isCommish: false,
+  },
+  {
+    id: "Bob-Angelas_Gang-1",
+    name: "Bob",
+    leagueId: "Angelas_Gang-1",
+    isCommish: false,
+  },
+]
