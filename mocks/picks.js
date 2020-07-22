e83ee5042405004988dae2021f36b29b25b5ef80
John Hamm <johndhamm1@gmail.com>
2020-07-21 20:00:29 -0500

    refactor(mocks): update mocks for testing BoardPage in app

--- mocks/picks.js
+++ mocks/picks.js
@@ -3,7 +3,7 @@ module.exports.mockNFLPicks = [
     id: "pick-1",
     selectionNumber: 1,
     leagueId: "Nashville_Volleyball-1",
-    ownerId: "John-Nashville_Volleyball-1",
+    ownerId: "Richard-Nashville_Volleyball-1",
     playerId: "DHenry-TEN-RB",
   },
   {
@@ -17,7 +17,28 @@ module.exports.mockNFLPicks = [
     id: "pick-3",
     selectionNumber: 3,
     leagueId: "Nashville_Volleyball-1",
-    ownerId: "Homer-Nashville_Volleyball-1",
+    ownerId: "Pat-Nashville_Volleyball-1",
     playerId: "JSmith-Schuster-WR",
+  },
+  {
+    id: "pick-4",
+    selectionNumber: 4,
+    leagueId: "Nashville_Volleyball-1",
+    ownerId: "Minh-Nashville_Volleyball-1",
+    playerId: "BRoethlisberger-QB",
+  },
+  {
+    id: "pick-5",
+    selectionNumber: 5,
+    leagueId: "Nashville_Volleyball-1",
+    ownerId: "Minh-Nashville_Volleyball-1",
+    playerId: "AKamara-NO-RB",
+  },
+  {
+    id: "pick-6",
+    selectionNumber: 6,
+    leagueId: "Nashville_Volleyball-1",
+    ownerId: "Pat-Nashville_Volleyball-1",
+    playerId: "ABrown-TEN-WR",
   },
 ]
