7f88c78248bab04091f3ddff21c8c7191c866b9a
John Hamm <johndhamm1@gmail.com>
2020-08-17 11:04:14 -0500

    feat(app): change CORS options

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const cors = require("cors")
+// const cors = require("cors")
 const http = require("http")
 const socketIo = require("socket.io")
 const mongoose = require("mongoose")
@@ -45,8 +45,29 @@ io.on("connect", (socket) => {
   })
 })
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  )
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true)
+  // Pass to next layer of middleware
+  next()
+})
+
+// app.use((req, res, next) => {
+//   res.set("Access-Control-Allow-Origin", "*")
+//   next()
+// })
 
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, {
